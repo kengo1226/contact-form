da9b231acbe5c510daac19b9cc4c5547f752fa38
kengo1226 <kengo1226size@gmail.com>
2022-11-03 01:00:58 +0900

    commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>お問い合わせフォーム</title>
+    <title>お問い合わせフォームSAMPLE</title>
 </head>
 <body>
     <form action="confirm.php" method="post">
